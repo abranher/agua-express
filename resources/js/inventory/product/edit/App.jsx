9cb0879a6ea901fc1932bf836e4334716bd5640e
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-09-17 02:37:18 -0400

    edit product ready

--- resources/js/inventory/product/edit/App.jsx
+++ resources/js/inventory/product/edit/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { APP_URL } from "../../../APP_URL";
-import SelectForm from "../../../components/SelectForm";
 import InputForm from "../../../components/InputForm";
 import TextareaForm from "../../../components/TextareaForm";
+import Button from "../../../components/Button";
 
 function App() {
   const {
@@ -228,12 +228,7 @@ function App() {
                 Descartar
               </button>
             </a>
-            <button
-              type="submit"
-              className="w-full sm:w-auto justify-center text-white inline-flex bg-violet-600 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:hover:bg-violet-600 active:bg-violet-800"
-            >
-              Actualizar
-            </button>
+            <Button>Actualizar</Button>
           </div>
         </section>
       </form>
