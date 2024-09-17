deb3ae6b707aacb6bb97094edd3d5b5a11574815
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-09-17 01:27:59 -0400

    add react

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-layout title="Panel Administrativo">
 
-   <x-navbar-dashboard />
+    <x-navbar-dashboard />
 
     <!-- Sidebar -->
 
@@ -15,13 +15,6 @@
         </div>
         <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"></div>
         <div class="grid grid-cols-2 gap-4 mb-4">
-            <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-            <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-            <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-            <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-        </div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"></div>
-        <div class="grid grid-cols-2 gap-4">
             <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
             <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
             <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
--- routes/admin.php
+++ routes/admin.php
@@ -10,7 +10,10 @@ Route::middleware(['auth', 'role:ADMIN'])->group(function () {
     return view('admin.dashboard');
   })->name('admin.dashboard');
 
-
+  Route::get('products/report-pdf', [ProductController::class, 'reportPDF'])
+    ->name('products.report-pdf');
+  Route::get('products/download-pdf', [ProductController::class, 'downloadPDF'])
+    ->name('products.download-pdf');
   Route::resource('products', ProductController::class);
 
   Route::resource('stock-products', StockProductController::class);
