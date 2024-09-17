2622d1f8dcc808b4a3042cb5918492c225697ee0
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-09-17 01:05:31 -0400

    fixed auth and roles

---
+++ app/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+  use HasFactory;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+    'description',
+    'price',
+    'min_stock',
+    'max_stock',
+    'image',
+    'image_2',
+    'image_3',
+    'hidden',
+  ];
+
+  /**
+   * Get all of the stocks for the Product
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\HasMany
+   */
+  public function stocks(): HasMany
+  {
+    return $this->hasMany(StockProduct::class);
+  }
+}
---
+++ app/Models/StockProduct.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class StockProduct extends Model
+{
+  use HasFactory;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'product_id',
+    'expires_at',
+    'status',
+  ];
+
+  /**
+   * Get the product that owns the StockProduct
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function product(): BelongsTo
+  {
+    return $this->belongsTo(Product::class);
+  }
+}
