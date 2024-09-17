af4e4c6ca7ab52145241a5bcc6e7e33dea13ea05
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-09-17 01:11:22 -0400

    fixed auth and roles

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StockProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:ADMIN'])->group(function () {
@@ -7,4 +9,9 @@ Route::middleware(['auth', 'role:ADMIN'])->group(function () {
   Route::get('/admin', function () {
     return view('admin.dashboard');
   })->name('admin.dashboard');
+
+
+  Route::resource('products', ProductController::class);
+
+  Route::resource('stock-products', StockProductController::class);
 });
