2d5e3b1f4384a7f08c582ab435ba9ff08740f62d
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-09-17 01:17:56 -0400

    add traits

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\hasPicture;
+use App\Traits\hasStock;
+use App\Traits\Paginable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
-  use HasFactory;
+  use HasFactory, Paginable, hasPicture, hasStock;
 
   /**
    * The attributes that are mass assignable.
--- resources/views/components/sidebar/index.blade.php
+++ resources/views/components/sidebar/index.blade.php
@@ -52,16 +52,12 @@
                 </button>
                 <ul id="dropdown-pages" class="hidden py-2 space-y-2">
                     <li>
-                        <a href="#"
-                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Settings</a>
+                        <a href="{{ route('products.index') }}"
+                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Producto</a>
                     </li>
                     <li>
                         <a href="#"
-                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Kanban</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Calendar</a>
+                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Stock</a>
                     </li>
                 </ul>
             </li>
